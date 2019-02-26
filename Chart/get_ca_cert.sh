dddc54b323ed071f7ca5df8168cb2722142e671e
Till Klocke <till.klocke@gmail.com>
2019-02-26 14:17:25 +0100

    We are now semi-manually creating certificates and requesting the CA certificate from kubernetes. The script get_ca_cert can automatically replace the string CA_BUNDLE in a file with the base64 encoded root cert of the current k8s cluster. issue-cert.sh automatically creates a secret containing a private and a ceritificate signed by the k8s cert authoririty with a name expected by the sqlbee helm chart

--- Chart/get_ca_cert.sh
+++ Chart/get_ca_cert.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}' | base64 -d > k8s-ca.pem
+K8S_CA_CERT=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
+
+if [ -f "values.yaml" ]; then
+  echo "Patching values.yaml"
+  sed -i "s/CA_BUNDLE/$K8S_CA_CERT/g" values.yaml
+else
+  echo "values.yaml not found, please set the value webhook.caBundle manually to"
+  echo $K8S_CA_CERT
+fi
